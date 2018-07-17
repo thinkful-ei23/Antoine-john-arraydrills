f3adcbcca9e64679f9e3e85e26b8c6104ec92e22
john <johnjacobcosgrove@gmail.com>
2018-07-17 14:22:12 -0400

    added hazard

--- drills2.js
+++ drills2.js
@@ -14,3 +14,45 @@ function goodbye(){
 
 repeat(hello, 5)
 repeat(goodbye, 5)
+
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+    // This is a "predicate function" - it's a function that only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+   const newArray = []
+   for(let i = 0; i < arr.length; i++){
+       if(fn(arr[i]) === true){
+           newArray.push(arr[i])
+       }
+   }
+   return newArray
+}
+
+
+function hazardWarningCreator(typeOfWarning){
+    let warningCounter = 0;
+   return function(location){
+        warningCounter++
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`)
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`)
+    }
+}
+
+const rockWarning = hazardWarningCreator('Rocks on the road!');
+const hurricane = hazardWarningCreator('I would not be outside right now')
+const blizzard = hazardWarningCreator('it might be cold')
+
+rockWarning('Montana')
+hurricane('FLorida')
+hurricane('Alabama')
+blizzard('California')
