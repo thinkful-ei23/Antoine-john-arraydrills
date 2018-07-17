b6c98230b7435d452a29d7756c5e45f047a93836
john <johnjacobcosgrove@gmail.com>
2018-07-17 12:00:48 -0400

    min max done

--- drills.js
+++ drills.js
@@ -1,20 +1,25 @@
 function max (numbers){
     let i = 0
-    let maxNum = 0
+    let maxNum = numbers[0]
     while(i <=numbers.length){
         if (maxNum < numbers[i]){
             maxNum = numbers[i]
-        }}
+        }
+    i++
+    }
          console.log(maxNum)
 }
 
 function min(numbers){
     let i = 0
-    let minNum = 0
+    let minNum = numbers[0]
     while(i <=numbers.length){
         if (minNum > numbers[i]){
             minNum = numbers[i]
-        }}
+            
+        }
+        i++
+    }
          console.log(minNum)
 }
 
